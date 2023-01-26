31c79abace695519defdb313b45fb97838a63d99
Russell Fisher <russellfisher@Russells-MBP.localdomain>
2023-01-25 17:35:57 -0700

    Reducers

--- elements_tutorial/src/App.js
+++ elements_tutorial/src/App.js
@@ -5,6 +5,7 @@ import DropdownPage from "./pages/DropdownPage";
 import Sidebar from "./components/Sidebar";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
     return (
@@ -25,6 +26,9 @@ function App() {
                 </Route>
                 <Route path="/table">
                     <TablePage/>
+                </Route>
+                <Route path="/counter">
+                    <CounterPage initialCount={1}/>
                 </Route>
             </div>
         </div>
