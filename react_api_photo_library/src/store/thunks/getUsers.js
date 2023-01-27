68ee3eb8f2ec914aecead08ea38c394d197231e5
Russell Fisher <russell.a.fisher@gmail.com>
2023-01-27 12:12:52 -0700

    Remove artificial load time

--- react_api_photo_library/src/store/thunks/getUsers.js
+++ react_api_photo_library/src/store/thunks/getUsers.js
@@ -4,16 +4,7 @@ import axios from "axios";
 const getUsers = createAsyncThunk('users/get', async () => {
     const response = await axios.get('http://localhost:3005/users');
 
-    // TODO: remove
-    await pause(1000);
-
     return response.data;
 });
-// TODO: remove
-const pause = (duration) => {
-    return new Promise((resolve) => {
-       setTimeout(resolve, duration);
-    });
-}
 
 export {getUsers};
