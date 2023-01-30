a94a03a92d288aa330e18e9741b60b8c827f17d8
Russell F <russell.a.fisher@gmail.com>
2023-01-30 11:20:56 -0700

    React toolkit queries part 1, add randomly generated albums to list with GET POST DELETE functionality

--- react_api_photo_library/src/components/AlbumsList.js
+++ react_api_photo_library/src/components/AlbumsList.js
@@ -1,6 +1,37 @@
+import {useGetAlbumsQuery, usePostAlbumMutation} from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
+
 function AlbumsList({user}) {
+    const {data, error, isLoading} = useGetAlbumsQuery(user);
+    const [addAlbum, results] = usePostAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    };
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton className="h-10 w-full" instances={3}/>
+    } else if (error) {
+        content = <div>Error loading albums</div>
+    } else {
+        content = data.map((album) => {
+            return <AlbumsListItem key={album.id} album={album}/>
+        });
+    }
+
     return (
-        <div>Favorite albums for {user.name}</div>
+        <div>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold">Favorite albums for {user.name}:</h3>
+                <Button loading={results.isLoading} onClick={handleAddAlbum}>
+                    Add Album
+                </Button>
+            </div>
+            <div>{content}</div>
+        </div>
     );
 }
 
---
+++ react_api_photo_library/src/components/AlbumsListItem.js
@@ -0,0 +1,29 @@
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import {GoTrashcan} from "react-icons/go";
+import {useDeleteAlbumMutation} from "../store";
+
+function AlbumsListItem({album}) {
+    const [deleteAlbum, results] = useDeleteAlbumMutation();
+
+    const handleDeleteAlbum = () => {
+        deleteAlbum(album);
+    }
+
+    const header = (
+        <>
+            <Button className="mr-2" loading={results.isLoading} onClick={handleDeleteAlbum}>
+                <GoTrashcan />
+            </Button>
+            {album.title}
+        </>
+    );
+
+    return (
+        <ExpandablePanel header={header}>
+            List of photos in the album
+        </ExpandablePanel>
+    );
+}
+
+export default AlbumsListItem;
